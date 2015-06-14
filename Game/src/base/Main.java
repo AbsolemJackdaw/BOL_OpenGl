5e21159a8189e1f6ac52e049c6a0d9cb78aca77d
ArtixAllMighty <robbeenaxel@hotmail.com>
2015-06-14 11:17:32 +0200

    /***/

--- Game/src/base/Main.java
+++ Game/src/base/Main.java
@@ -19,33 +19,38 @@ import Utils.Log;
 
 
 public class Main {
-	
+
 	public static Window window;
 
 	public static int WIDTH = 1280;
 	public static int HEIGHT = 600;
-	
+
 	public static void main(String[] args) {
-		
+
 		window = createWindow(WIDTH, HEIGHT, "Brim Of Life");
-		
+
 		Game game = new Game();
-		
+
 		while(glfwWindowShouldClose(window.getWindowID()) == GL_FALSE) {
+
+
 			game.update();
 			game.render();
 			glfwPollEvents();
 			glfwSwapBuffers(window.getWindowID());
+
+
+
 		}
-		
+
 		game.dispose();
-		
+
 		window.getErrorCallBack().release();
 		window.getKeyCallback().release();
-		
+
 		glfwDestroyWindow(window.getWindowID());
 		glfwTerminate();
-		
+
 		System.exit(0);
 	}
 
@@ -54,31 +59,31 @@ public class Main {
 			Log.err("Error initialising glfw window");
 			System.exit(1);
 		}
-		
+
 		ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-		
+
 		glfwWindowHint(GLFW_SAMPLES, 4);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-		
+
 		long windowID = glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
-		
+
 		glfwSetWindowPos(
-                windowID,
-                (GLFWvidmode.width(vidmode) - WIDTH) / 2,
-                (GLFWvidmode.height(vidmode) - HEIGHT) / 2
-            );   
-		
+				windowID,
+				(GLFWvidmode.width(vidmode) - WIDTH) / 2,
+				(GLFWvidmode.height(vidmode) - HEIGHT) / 2
+				);   
+
 		GLFWErrorCallback errorCallBack;
 		GLFWKeyCallback keyCallBack;
-		
+
 		if (windowID == NULL) {
 			System.exit(1);
 		}
-		
+
 		glfwMakeContextCurrent(windowID);
 		GLContext.createFromCurrent();
 		glfwSwapInterval(1);
@@ -88,9 +93,9 @@ public class Main {
 				onKeyInvoke(window, key, scancode, action, mods);
 			}
 		});
-		
+
 		glfwSetErrorCallback(errorCallBack = Callbacks.errorCallbackPrint(System.err));
-		
+
 		return new Window(windowID, keyCallBack, errorCallBack);
 	}
 
